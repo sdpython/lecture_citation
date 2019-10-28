160773aa823fc6ab9f6538353d81aacd6b8f1aa4
xavier dupré <xavier.dupre@gmail.com>
2019-10-28 15:18:51 +0100

    Update test_readme.py

--- _unittests/ut_module/test_readme.py
+++ _unittests/ut_module/test_readme.py
@@ -28,7 +28,7 @@ class TestReadme(unittest.TestCase):
             # does not work from a virtual environment
             return
 
-        from pyquickhelper.pycode import check_readme_syntax
+        from pyquickhelper.pycode import check_readme_syntax  # pylint: disable=C0415
 
         check_readme_syntax(readme, folder=temp, fLOG=fLOG)
 
