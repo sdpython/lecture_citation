0b84ca6fa401825606b3524c7d418f66c51ffc91
xavier dupré <xavier.dupre@gmail.com>
2020-06-22 10:03:55 +0200

    documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-# import sphinx_modern_theme_modified
 import alabaster
-# import sphinx_redactor_theme
 from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
 import lecture_citation
 
@@ -13,7 +11,6 @@ local_template = os.path.join(os.path.abspath(
 
 set_sphinx_variables(__file__, "lecture_citation", "Xavier Dupré", 2020,
                      "alabaster", alabaster.get_path(),
-                     # "sphinx_redactor_theme", [sphinx_redactor_theme.get_html_theme_path()],
                      locals(), extlinks=dict(
                          issue=('https://github.com/sdpython/lecture_citation/issues/%s', 'issue')),
                      title="Souvenirs de lecture", book=True, nblayout='table')
@@ -21,73 +18,9 @@ set_sphinx_variables(__file__, "lecture_citation", "Xavier Dupré", 2020,
 blog_root = "http://www.xavierdupre.fr/app/lecture_citation/helpsphinx/"
 html_search_language = "fr"
 pygments_style = 'sphinx'
-# blockdiag_fontpath = '/usr/share/fonts/truetype/ipafont/ipagp.ttf'
-# extensions.extend(['alabaster'])
-
-html_context = {
-    'css_files': get_default_stylesheet() + ['_static/my-styles.css'],
-}
-
+html_context = {'css_files': get_default_stylesheet() + ['_static/my-styles.css']}
 html_logo = "phdoc_static/project_ico.png"
-
 language = "fr"
-
-preamble = '''
-\\usepackage{etex}
-\\usepackage{fixltx2e} % LaTeX patches, \\textsubscript
-\\usepackage{cmap} % fix search and cut-and-paste in Acrobat
-\\usepackage[raccourcis]{fast-diagram}
-\\usepackage{titlesec}
-\\usepackage{amsmath}
-\\usepackage{amssymb}
-\\usepackage{amsfonts}
-\\usepackage{graphics}
-\\usepackage{epic}
-\\usepackage{eepic}
-%\\usepackage{pict2e}
-%%% Redefined titleformat
-\\setlength{\\parindent}{0cm}
-\\setlength{\\parskip}{1ex plus 0.5ex minus 0.2ex}
-\\newcommand{\\hsp}{\\hspace{20pt}}
-\\newcommand{\\acc}[1]{\\left\\{#1\\right\\}}
-\\newcommand{\\cro}[1]{\\left[#1\\right]}
-\\newcommand{\\pa}[1]{\\left(#1\\right)}
-\\newcommand{\\R}{\\mathbb{R}}
-\\newcommand{\\HRule}{\\rule{\\linewidth}{0.5mm}}
-%\\titleformat{\\chapter}[hang]{\\Huge\\bfseries\\sffamily}{\\thechapter\\hsp}{0pt}{\\Huge\\bfseries\\sffamily}
-'''
-
-custom_preamble = """\n
-\\usepackage[all]{xy}
-\\newcommand{\\vecteur}[2]{\\pa{#1,\\dots,#2}}
-\\newcommand{\\N}[0]{\\mathbb{N}}
-\\newcommand{\\indicatrice}[1]{\\mathbf{1\\!\\!1}_{\\acc{#1}}}
-\\newcommand{\\infegal}[0]{\\leqslant}
-\\newcommand{\\supegal}[0]{\\geqslant}
-\\newcommand{\\ensemble}[2]{\\acc{#1,\\dots,#2}}
-\\newcommand{\\fleche}[1]{\\overrightarrow{ #1 }}
-\\newcommand{\\intervalle}[2]{\\left\\{#1,\\cdots,#2\\right\\}}
-\\newcommand{\\loinormale}[2]{{\\cal N}\\pa{#1,#2}}
-\\newcommand{\\independant}[0]{\\;\\makebox[3ex]{\\makebox[0ex]{\\rule[-0.2ex]{3ex}{.1ex}}\\!\\!\\!\\!\\makebox[.5ex][l]{\\rule[-.2ex]{.1ex}{2ex}}\\makebox[.5ex][l]{\\rule[-.2ex]{.1ex}{2ex}}} \\,\\,}
-\\newcommand{\\esp}{\\mathbb{E}}
-\\newcommand{\\var}{\\mathbb{V}}
-\\newcommand{\\pr}[1]{\\mathbb{P}\\pa{#1}}
-\\newcommand{\\loi}[0]{{\\cal L}}
-\\newcommand{\\vecteurno}[2]{#1,\\dots,#2}
-\\newcommand{\\norm}[1]{\\left\\Vert#1\\right\\Vert}
-\\newcommand{\\norme}[1]{\\left\\Vert#1\\right\\Vert}
-\\newcommand{\\dans}[0]{\\rightarrow}
-\\newcommand{\\partialfrac}[2]{\\frac{\\partial #1}{\\partial #2}}
-\\newcommand{\\partialdfrac}[2]{\\dfrac{\\partial #1}{\\partial #2}}
-\\newcommand{\\loimultinomiale}[1]{{\\cal M}\\pa{#1}}
-\\newcommand{\\trace}[1]{tr\\pa{#1}}
-\\newcommand{\\sac}[0]{|}
-\\newcommand{\\abs}[1]{\\left|#1\\right|}
-"""
-# \\usepackage{eepic}
-
-imgmath_latex_preamble = preamble + custom_preamble
-latex_elements['preamble'] = preamble + custom_preamble
 mathdef_link_only = True
 
 epkg_dictionary.update({
