34b880f5b03732a23e388fcff8b560e38f0e4734
xavier dupré <xavier.dupre@gmail.com>
2021-05-27 02:27:17 +0200

    documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -2,7 +2,7 @@
 import sys
 import os
 import alabaster
-from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
+from pyquickhelper.helpgen.default_conf import set_sphinx_variables
 import lecture_citation
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
@@ -18,8 +18,7 @@ set_sphinx_variables(__file__, "lecture_citation", "Xavier Dupré", 2021,
 blog_root = "http://www.xavierdupre.fr/app/lecture_citation/helpsphinx/"
 html_search_language = "fr"
 pygments_style = 'sphinx'
-html_context = {'css_files': get_default_stylesheet() +
-                ['_static/my-styles.css']}
+html_css_files = ['my-styles.css']
 html_logo = "phdoc_static/project_ico.png"
 language = "fr"
 mathdef_link_only = True
